bc9613d6b46ac59cc76ec70d5fb3b7e7254527a1
Eirik Brandtzæg <eirikb@eirikb.no>
2010-10-16 19:23:55 +0200

    License added to gwt project

--- example/gwt-channel-api/src/no/eirikb/gwtchannelapi/client/GwtChannelApi.java
+++ example/gwt-channel-api/src/no/eirikb/gwtchannelapi/client/GwtChannelApi.java
@@ -1,3 +1,26 @@
+/**
+ * Copyright (c) 2010, Eirik Brandtzæg
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *  *  Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *  *  Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL EIRIK BRANDTZÆG BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 package no.eirikb.gwtchannelapi.client;
 
 import com.google.gwt.core.client.EntryPoint;
@@ -93,7 +116,7 @@ public class GwtChannelApi implements EntryPoint {
 		String text = chat.getText();
 		chat.setText(text.length() > 0 ? text + '\n' + line : line);
 	}
-	
+
 	native void join(String channelKey) /*-{
 		var c = new $wnd.goog.appengine.Channel(channelKey);
 		var socket = c.open();
