501b728b8ddeb4dea067d59a7a6d2b7adc166d2d
eirikb <eirikb@eirikb.no>
2013-02-26 01:30:03 +0100

    Removed unused Service Path in dummy service

--- src/main/java/no/eirikb/gwtchannelapi/client/DummySerializeService.java
+++ src/main/java/no/eirikb/gwtchannelapi/client/DummySerializeService.java
@@ -3,13 +3,11 @@ package no.eirikb.gwtchannelapi.client;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.rpc.IsSerializable;
 import com.google.gwt.user.client.rpc.RemoteService;
-import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
 
-@RemoteServiceRelativePath("channel_service")
 public interface DummySerializeService extends RemoteService {
     IsSerializable getMessage(IsSerializable message);
 }
 
- interface DummySerializeServiceAsync {
+interface DummySerializeServiceAsync {
     void getMessage(IsSerializable message, AsyncCallback<IsSerializable> callback);
 }
